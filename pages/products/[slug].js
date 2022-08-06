75016f84b35d208a5a57dad841738cf50d69b46c
Abhishek Tomar <itsabhi.at@gmail.com>
2022-08-07 00:51:07 +0530

    single-product/added

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -2,7 +2,8 @@ import { useRouter } from "next/router";
 import React from "react";
 import { useQuery } from "urql";
 import { GET_PRODUCT_QUERY } from "../../lib/query";
-
+import styled from "styled-components";
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 function ProductDetails() {
   const { query } = useRouter();
 
@@ -15,23 +16,81 @@ function ProductDetails() {
 
   if (fetching) return <p>Loading..</p>;
   if (error) return <p>error it is</p>;
-  console.log(data);
+  const { title, description, image } = data.products.data[0].attributes;
   return (
-    <div>
-      <img src="" alt="" />
-      <div>
-        <h3>Title</h3>
-        <p>description</p>
-        <div>
+    <DetailsStyle>
+      <img src={image.data.attributes.formats.medium.url} alt={title} />
+      <ProductInfo>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <Quantity>
           <span>Quatity</span>
-          <button>plus</button>
+          <button>
+            <AiFillMinusCircle />
+          </button>
           <p>0</p>
-          <button>Minus</button>
-        </div>
-        <button>Add to Cart</button>
-      </div>
-    </div>
+          <button>
+            <AiFillPlusCircle />
+          </button>
+        </Quantity>
+        <Buy>Add to Cart</Buy>
+      </ProductInfo>
+    </DetailsStyle>
   );
 }
 
 export default ProductDetails;
+
+const DetailsStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5rem;
+  img {
+    width: 40%;
+  }
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    img {
+      width: 100%;
+    }
+  }
+`;
+const ProductInfo = styled.div`
+  width: 40%;
+  button {
+    font-size: 1rem;
+    font-weight: medium;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+  }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+const Quantity = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem 0rem;
+  button {
+    background: transparent;
+    border: none;
+    display: flex;
+    font-size: 1.5rem;
+  }
+  p {
+    width: 1rem;
+    text-align: center;
+  }
+  span {
+    color: var(--secondary);
+  }
+  svg {
+    color: #494949;
+  }
+`;
+const Buy = styled.button`
+  width: 100%;
+  background: var(--primary);
+  color: white;
+  font-weight: 500;
+`;
