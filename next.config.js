b940965f15c5304783fe1ce8ab4e08c6d07b179a
Abhishek Tomar <itsabhi.at@gmail.com>
2022-10-31 19:37:45 +0530

    auth 0 added

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_STRIPE_SECRET_KEY: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
-  },
+  // env: {
+  //   NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
+  //   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+  //     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  //   NEXT_PUBLIC_STRIPE_SECRET_KEY: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
+  // },
   swcMinify: true,
   compiler: {
     styledComponents: true,
