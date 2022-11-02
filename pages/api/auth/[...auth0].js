eaa87dbec5154af07d46b2241e07f62921016cbe
Abhishek Tomar <itsabhi.at@gmail.com>
2022-11-02 16:06:06 +0530

    minor change

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,13 +1,3 @@
 import { handleAuth } from "@auth0/nextjs-auth0";
-import auth0 from "src/util/auth0";
 
-export default handleAuth({
-  // other stuff here...
-  async callback(req, res) {
-    try {
-      await auth0.handleCallback(req, res);
-    } catch (error) {
-      res.redirect("/");
-    }
-  },
-});
+export default handleAuth();
